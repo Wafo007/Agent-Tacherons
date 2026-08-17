4e8c9395af8cc06888157cb0c0bb0fae6c056053
Wafo007 <aroldwafo50@gmail.com>
2026-08-17 05:25:40 +0100

    fix: adresse for Render

--- w4fo_app/lib/core/constants/app_constants.dart
+++ w4fo_app/lib/core/constants/app_constants.dart
@@ -3,7 +3,7 @@
 /// En développement, le backend tourne en local. En production, ces valeurs
 /// doivent être injectées via `--dart-define` plutôt que codées en dur ici
 /// (voir commentaire dans `main.dart`).
-class AppConstants {
+/*class AppConstants {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
     defaultValue: 'http://localhost:8000',
@@ -12,6 +12,22 @@ class AppConstants {
   static const String wsBaseUrl = String.fromEnvironment(
     'WS_BASE_URL',
     defaultValue: 'ws://localhost:8000',
+  );
+
+  static const String secureStorageAccessTokenKey = 'w4fo_access_token';
+  static const String secureStorageRefreshTokenKey = 'w4fo_refresh_token';
+}*/
+
+/// Constantes globales de l'application.
+class AppConstants {
+  static const String apiBaseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'https://agent-tacherons.onrender.com',
+  );
+
+  static const String wsBaseUrl = String.fromEnvironment(
+    'WS_BASE_URL',
+    defaultValue: 'wss://agent-tacherons.onrender.com',
   );
 
   static const String secureStorageAccessTokenKey = 'w4fo_access_token';
